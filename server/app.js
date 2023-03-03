d2c6a078288aeeca875e966eba4c07c4532c07a0
rakib3004 <bsse1129@iit.du.ac.bd>
2023-03-03 10:17:44 +0600

    add new app.js indicator

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const authorRouter = require('./routes/author.route');
+const blogRouter =  require('./routes/blog.route');
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json({extended: false}));
+
+app.use('/author',authorRouter);
+app.use('/blog', blogRouter);
+
+app.use((err, req, res, next) => {
+    if(err.name == 'ValidationError'){
+        var valErrors = [];
+        Object.keys(err.errors).forEach( key => valErrors.push(err.errors[key].message) );
+        res.status(422).send(valErrors)
+    }
+});
+
+
+
