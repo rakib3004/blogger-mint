9b268643fddabee59abfb64f5213dffe15a2fd2a
rakib3004 <bsse1129@iit.du.ac.bd>
2023-03-03 10:22:55 +0600

    add all files in this system

--- server/app.js
+++ server/app.js
@@ -12,10 +12,13 @@ app.use('/blog', blogRouter);
 app.use((err, req, res, next) => {
     if(err.name == 'ValidationError'){
         var valErrors = [];
+    valErrors.push('Server Crashed!!!')
         Object.keys(err.errors).forEach( key => valErrors.push(err.errors[key].message) );
         res.status(422).send(valErrors)
     }
 });
 
 
-
+app.listen(PORT, ()=>{
+    console.log('Server is running ... ... ...');
+})
