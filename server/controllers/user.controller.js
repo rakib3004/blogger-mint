e07b11a092f987a324431136d367329396e3f91e
rakib3004 <bsse1129@iit.du.ac.bd>
2023-03-06 09:01:57 +0600

    add functionality in user service

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -8,7 +8,7 @@ async function getUser(req, res) {
             res.send({ status: 402, message: "null user" });
             return;
         }
-        res.json(await statusService.getUser(uid));
+        res.json(await userService.getUser(uid));
     } catch (err) {
         console.error(err);
         res.send({ status: 500, message: err });
@@ -19,7 +19,7 @@ async function getUser(req, res) {
 async function getAllUser(req, res) {
     try {
        
-        res.json(await statusService.getUser());
+        res.json(await userService.getAllUser());
     } catch (err) {
         console.error(err);
         res.send({ status: 500, message: err });
@@ -28,13 +28,43 @@ async function getAllUser(req, res) {
 
 async function createUser(req, res) {
    
+    try {
+        
+        res.json(await userService.createUser());
+    } catch (err) {
+        console.error(err);
+        res.send({ status: 500, message: err });
+    }
+
 }
 
 
 async function updateUser(req, res) {
    
+    try {
+        uid = req.body.id;
+        if (uid == null) {
+            res.send({ status: 402, message: "null user" });
+            return;
+        }
+        res.json(await userService.updateUser(uid));
+    } catch (err) {
+        console.error(err);
+        res.send({ status: 500, message: err });
+    }
+
 }
 
 async function deleteUser(req, res) {
-   
+   try {
+        uid = req.body.id;
+        if (uid == null) {
+            res.send({ status: 402, message: "null user" });
+            return;
+        }
+        res.json(await userService.deleteUser(uid));
+    } catch (err) {
+        console.error(err);
+        res.send({ status: 500, message: err });
+    }
 }
