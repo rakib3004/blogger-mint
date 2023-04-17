9f9adb14390d59b8050634ee05f3fca8166902d8
rakib3004 <bsse1129@iit.du.ac.bd>
2023-04-17 16:34:07 +0600

    fix/issue-1-improvement: add cors credentials is true

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(cors(
     origin: [process.env.CROSS_ORIGIN_1, process.env.CROSS_ORIGIN_2],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+  credentials: true,
 
   }
 ));
