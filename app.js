35999f390bfa033e125311b969230931ace5e1d5
rakib3004 <bsse1129@iit.du.ac.bd>
2023-05-10 18:31:18 +0600

    fix/issue-1-improvement: unforgetable day of react nodejs error

--- app.js
+++ app.js
@@ -1,38 +1,26 @@
+
 require("dotenv").config();
 const express = require('express');
 const app = express();
 const indexRouter = require('./routes/index.route');
 const cookieParser = require('cookie-parser');
-const morgan = require('morgan')
 
 const PORT = process.env.SERVER_PORT;
-const HOST = process.env.SERVER_HOST;
 const cors = require('cors');
+app.use(cors(
+  {
+    origin: [process.env.CROSS_ORIGIN_1, process.env.CROSS_ORIGIN_2],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+  credentials: true,
 
-app.use(morgan('dev'))
+  }
+));
+app.use(express.json());
+app.use(cookieParser());
 app.use("/api/v1", indexRouter);
 
 
-app.use(cors(
-  {
-    origin: ['http://localhost:5173','http://127.0.0.1:5173', process.env.CROSS_ORIGIN_1, process.env.CROSS_ORIGIN_2,process.env.CROSS_ORIGIN_3,],
-    // origin: '*',
-    // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true,
-
-    // allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
-    // exposedHeaders: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials'],
-  },
-  console.log("----uiiu----")
-));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-  });
-// app.use(cors())
-app.use(cookieParser());
-app.use(express.json());
 
 app.use((err, req, res, next) => {
   if (!err) {
@@ -53,6 +41,6 @@ app.use('*', (req, res) => {
   });
 });
 
-app.listen( PORT, HOST, () => {
-  console.log('---log---',HOST, PORT);
+
+app.listen(PORT, () => {
 });
